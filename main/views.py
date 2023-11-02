7224934362486abbcc55bf07677ff862c7dbc54c
jsvlog <lizardojohnsonc@gmail.com>
2023-11-02 19:16:51 +0800

    login

--- main/views.py
+++ main/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been log in")
+            return redirect('home')
+        else:
+            messages.success(request, "there was an error logging in!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logout!")
+    return redirect('home')
